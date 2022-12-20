e3c078e2b2295a78b2d8ca2484bf852410a7a472
Dhyunlee <skyblue5030@naver.com>
2022-12-21 03:20:38 +0900

    Feat: 메인 페이지 구현

---
+++ main.js
@@ -0,0 +1,29 @@
+// use swiper
+new Swiper(".banner-container", {
+  effect: 'fade',
+  fadeEffect: {
+    slideShadows: false,
+  },
+  speed: 400,
+  spaceBetween: 100,
+  autoplay: {
+    delay: 5000,
+  },
+  pagination: {
+    el: ".banner-pagination",
+    clickable: true
+  },
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev ',
+  },
+  spaceBetween: 0, // 이미지 사이  공백 없애기(= mergin 없앰) 
+  // 무한 슬라이드
+  loop: true,
+  loopAdditionalSlides: 2,
+});
+
+// 우클릭 방지
+document.addEventListener('contextmenu', (e) => {
+    e.preventDefault()
+})
